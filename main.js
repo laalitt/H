4f113b357a7792ec3ba8a176c8fd888a28bced01
laalitt <72173027+laalitt@users.noreply.github.com>
2021-06-25 17:20:43 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,20 +1,15 @@
-function setup() {
-    canvas = createCanvas(640,400);
-    canvas.position(150, 150);
-    video = createCapture(VIDEO);
-    video.hide();
+function up() {
+    Move up 
 }
 
-function draw() {
-    image(video, 250, 150, 220, 200);
+function down() {
+    Move down 
+}
 
-    fill(255, 0, 0);
-    stroke(255, 0, 0);
-    circle(50,50,80);
+function left() {
+    Move left 
+}
 
-    fill(0, 128, 0);
-    stroke(0, 128, 0);
-    rect(90, 40,460,20);
-
-    rect(90, 420, 460, 20);
+function right() {
+    Move right 
 }
